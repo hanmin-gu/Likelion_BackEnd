23d7cc6fc0b4f22302dcb0b08156156960186c4a
hanmin-gu <mingu9968@gmail.com>
2023-09-18 17:30:05 +0900

    init:

--- user-api/src/main/java/com/example/config/CorsConfig.java
+++ user-api/src/main/java/com/example/config/CorsConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 
 public class CorsConfig {
 
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
